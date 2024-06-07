fbc8c548795c713bc38bdcb2f94d5c581c811701
siriiane <sirine.benghazala@lepoles.org>
2024-06-07 17:07:51 +0200

    premier commit

---
+++ public/header.html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+
+<body>
+<div id="content">
+    <header>
+
+        <nav>
+            <h1>Ce Soir</h1>
+            <ul id="lien">
+                <li>our story</li>
+                <li>fare</li>
+                <li>drink</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+        <div><!-- <img src="/ce_soir/img/cerf.svg" width="100" id="cerf"> -->
+        </div>
+        <div>
+
+        </div>
+    </header>
